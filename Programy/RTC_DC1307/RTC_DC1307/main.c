37528d0bc86701ba64d4aa78f5d5342a25c9a808
student <student@PC-629.urel.feec.vutbr.cz>
2021-11-30 14:25:10 +0100

    changed

---
+++ Programy/RTC_DC1307/RTC_DC1307/main.c
@@ -0,0 +1,18 @@
+/*
+ * RTC_DC1307.c
+ *
+ * Created: 30.11.2021 14:21:36
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
